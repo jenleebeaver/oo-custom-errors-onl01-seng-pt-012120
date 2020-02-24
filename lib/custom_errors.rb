22b6dacc98e937ced9dc1997c6325ec1aeb56c72
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-02-24 16:52:09 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,7 +8,7 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      begin 
+      begin
         raise PartnerError
       rescue PartnerError => error
       raise PartnerError
